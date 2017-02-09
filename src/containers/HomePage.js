5b136cc9d56c9ed0d4e92e0febc4ecde641ac77d
“juventusyangfan <yangfan1987816@gmail.com>
2017-02-09 16:48:21 +0800

    add snackbar component

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -5,3 +5,5 @@
 import React, { Component, PropTypes } from "react"
 import { connect } from "react-redux";
 import {selectTab,fetchTopics,recordScrollT} from "../actions";
+import Header from "../components/HomePage/Header/Header";
+import Lists from "../components/HomePage/Lists/Lists";
