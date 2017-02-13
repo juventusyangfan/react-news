83e683638d8d46c2b2689969f1a3cb0a03c58cf2
“juventusyangfan <yangfan1987816@gmail.com>
2017-02-13 09:46:31 +0800

    修改样式

--- src/components/HomePage/Header/Header.js
+++ src/components/HomePage/Header/Header.js
@@ -6,9 +6,18 @@ import React, { PropTypes,Component } from "react";
 import styles from "./Header.scss";
 import {Link} from "react-router";
 import {Tabs, Tab} from "material-ui/Tabs";
+import {grey800} from "material-ui/styles/colors";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import muiThemeable from "material-ui/styles/muiThemeable";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 import SwipeableViews from "react-swipeable-views";
+
+const muiTheme = getMuiTheme({
+    palette: {
+        backgroundColor: grey800
+    }
+});
 
 class Header extends Component {
     constructor(props) {
@@ -40,14 +49,15 @@ class Header extends Component {
 
     render() {
         return (
-            <MuiThemeProvider>
+            <MuiThemeProvider muiTheme={muiTheme}>
                 <div>
                     <div className={styles.header} style={{top:-this.props.fixedTop}}>
-                        <AppBar title={<p style={{textAlign:'center'}}>体育新闻</p>}/>
+                        <AppBar title={<p style={{textAlign:'center'}}>体育新闻</p>}
+                                style={{backgroundColor:muiTheme.palette.backgroundColor}}/>
                         <Tabs onChange={this.handleChange} value={this.state.slideIndex}>
                             {this.props.tabs.map((tab, i) =>
-                                <Tab key={i} label={tab.title} value={i}>
-                                </Tab>
+                                    <Tab key={i} label={tab.title} value={i}>
+                                    </Tab>
                             )}
                         </Tabs>
                     </div>
@@ -60,4 +70,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default muiThemeable()(Header);
